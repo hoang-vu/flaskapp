0b9f11e7c13eb2a688612297d23e1a837fd3309e
hoang vu <stm12p2@gmail.com>
2018-04-23 12:18:08 +1000

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+"""Filename: app.py
+  """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world(username=None):
+    return("Hello")
+
+if __name__ == "__main__":
+    app.run()
